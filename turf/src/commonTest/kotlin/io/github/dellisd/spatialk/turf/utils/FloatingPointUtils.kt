7a27a3830ba529fbb025f50828c76461d6341b33
Derek Ellis <derekellis@cmail.carleton.ca>
2020-12-06 15:00:21 -0500

    Line utils (#72)
    * Upgrade to Kotlin 1.4.20
    Update serialization dependency
    
    * Add lineIntersect, lineSlice, and nearestPointOnLine
    
    Fix buffer issue in Resource readText function

--- turf/src/commonTest/kotlin/io/github/dellisd/spatialk/turf/utils/FloatingPointUtils.kt
+++ turf/src/commonTest/kotlin/io/github/dellisd/spatialk/turf/utils/FloatingPointUtils.kt
@@ -1,5 +1,6 @@
 package io.github.dellisd.spatialk.turf.utils
 
+import io.github.dellisd.spatialk.geojson.Position
 import kotlin.math.abs
 import kotlin.test.asserter
 
@@ -9,4 +10,11 @@ fun assertDoubleEquals(expected: Double, actual: Double?, epsilon: Double, messa
         { (message ?: "") + "Expected <$expected>, actual <$actual>, should differ no more than <$epsilon>." },
         abs(expected - actual!!) <= epsilon
     )
+}
+
+fun assertPositionEquals(expected: Position, actual: Position?, epsilon: Double = 0.0001, message: String? = null) {
+    asserter.assertNotNull(null, actual)
+
+    assertDoubleEquals(expected.latitude, actual?.latitude, epsilon, message)
+    assertDoubleEquals(expected.longitude, actual?.longitude, epsilon, message)
 }
--- turf/src/nativeTest/kotlin/io/github/dellisd/spatialk/turf/utils/Resource.kt
+++ turf/src/nativeTest/kotlin/io/github/dellisd/spatialk/turf/utils/Resource.kt
@@ -5,10 +5,12 @@ import kotlinx.cinterop.CPointer
 import kotlinx.cinterop.allocArray
 import kotlinx.cinterop.convert
 import kotlinx.cinterop.memScoped
+import kotlinx.cinterop.set
 import kotlinx.cinterop.sizeOf
 import kotlinx.cinterop.toKString
 import platform.posix.FILE
 import platform.posix.SEEK_END
+import platform.posix.fclose
 import platform.posix.fopen
 import platform.posix.fread
 import platform.posix.fseek
@@ -16,7 +18,7 @@ import platform.posix.ftell
 import platform.posix.rewind
 
 actual class Resource actual constructor(actual val name: String) {
-    private val file: CPointer<FILE>? = fopen("$RESOURCE_PATH/$name", "r")
+    private val file: CPointer<FILE>? = fopen("$RESOURCE_PATH/$name", "rb")
 
     actual fun exists(): Boolean = file != null
 
@@ -26,8 +28,12 @@ actual class Resource actual constructor(actual val name: String) {
         rewind(file)
 
         return memScoped {
-            val tmp = allocArray<ByteVar>(size)
+            val tmp = allocArray<ByteVar>(size + 1)
             fread(tmp, sizeOf<ByteVar>().convert(), size.convert(), file)
+            fclose(file)
+
+            // null terminate string
+            tmp[size] = 0
             tmp.toKString()
         }
     }
