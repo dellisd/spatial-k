72865fc1d487a1123d8c4bc774845cdf0cf7eaf7
Derek <derek@llamabagel.ca>
2021-03-17 18:45:01 -0400

    Add `bearingToAzimuth` method
    Remove placeholder files

--- geojson-dsl/src/iosMain/kotlin/Placeholder.kt
+++
@@ -1,4 +0,0 @@
-// KT-36674
-private fun placeholder2() {
-    println("Placeholder until Kotlin 1.4")
-}
--- geojson-dsl/src/nativeMain/kotlin/Placeholder.kt
+++
@@ -1,4 +0,0 @@
-// KT-36674
-private fun placeholder() {
-    println("Placeholder until Kotlin 1.4")
-}
