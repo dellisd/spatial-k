3ff6396f35d0746db170e26e0cf99f5f15bf5c72
Derek Ellis <derekellis170@gmail.com>
2022-04-15 12:29:30 -0400

    Add benchmarking setup for geojson

--- geojson/build.gradle.kts
+++ geojson/build.gradle.kts
@@ -3,48 +3,31 @@ plugins {
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.dokka)
     alias(libs.plugins.publish)
+    alias(libs.plugins.kotlinx.benchmark)
 }
 
 kotlin {
-    jvm()
+    jvm {
+        compilations.create("bench")
+    }
     js {
         browser {
         }
         nodejs {
         }
+
+        compilations.create("bench")
     }
     // For ARM, should be changed to iosArm32 or iosArm64
     // For Linux, should be changed to e.g. linuxX64
     // For MacOS, should be changed to e.g. macosX64
     // For Windows, should be changed to e.g. mingwX64
-    linuxX64("native")
+    linuxX64("native") {
+        compilations.create("bench")
+    }
     mingwX64("mingw")
     macosX64("macos")
     ios("ios")
-
-    sourceSets["commonMain"].dependencies {
-        api(libs.kotlinx.serialization)
-    }
-
-    sourceSets["commonTest"].dependencies {
-        implementation(kotlin("test"))
-        implementation(kotlin("test-annotations-common"))
-    }
-
-    sourceSets["jvmMain"].dependencies {
-    }
-
-    sourceSets["jvmTest"].dependencies {
-    }
-
-    sourceSets["jsMain"].dependencies {
-    }
-
-    sourceSets["jsTest"].dependencies {
-    }
-
-    sourceSets["nativeMain"].dependencies {}
-    sourceSets["nativeTest"].dependencies {}
 
     sourceSets {
         all {
@@ -56,15 +39,71 @@ kotlin {
             }
         }
 
-        val nativeMain by getting {}
-        getByName("macosMain").dependsOn(nativeMain)
-        getByName("iosMain").dependsOn(nativeMain)
-        getByName("mingwMain").dependsOn(nativeMain)
+        val commonMain by getting {
+            dependencies {
+                api(libs.kotlinx.serialization)
+            }
+        }
 
-        val nativeTest by getting {}
-        getByName("macosTest").dependsOn(nativeTest)
-        getByName("iosTest").dependsOn(nativeTest)
-        getByName("mingwTest").dependsOn(nativeTest)
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+
+        val jsMain by getting {}
+
+        val jvmMain by getting {}
+
+        val nativeMain by getting {
+            getByName("macosMain").dependsOn(this)
+            getByName("iosMain").dependsOn(this)
+            getByName("mingwMain").dependsOn(this)
+        }
+
+        val nativeTest by getting {
+            getByName("macosTest").dependsOn(this)
+            getByName("iosTest").dependsOn(this)
+            getByName("mingwTest").dependsOn(this)
+        }
+
+        val commonBench by creating {
+            dependsOn(commonMain)
+            dependencies {
+                implementation(libs.kotlinx.benchmark)
+            }
+        }
+
+        val jsBench by getting {
+            dependsOn(commonBench)
+            dependsOn(jsMain)
+        }
+
+        val jvmBench by getting {
+            dependsOn(commonBench)
+            dependsOn(jvmMain)
+        }
+
+        val nativeBench by getting {
+            dependsOn(commonBench)
+            dependsOn(nativeMain)
+        }
+    }
+}
+
+benchmark {
+    this.configurations {
+        getByName("main") {
+            iterations = 5
+        }
+    }
+
+    targets {
+        register("jvmBench")
+        // Broken on 1.6.20???
+        // register("jsBench")
+        register("nativeBench")
     }
 }
 
