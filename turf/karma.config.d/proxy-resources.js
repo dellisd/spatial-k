7fb06978260b14f85dfa85a2f12745a3c3b9bb2d
Derek Ellis <derekellis170@gmail.com>
2024-02-19 14:43:26 -0500

    Upgrade kotlinx-resources

---
+++ turf/karma.config.d/proxy-resources.js
@@ -0,0 +1,20 @@
+config.files.push({
+   pattern: __dirname + "/src/jsTest/resources/**",
+   watched: false,
+   included: false,
+   served: true,
+   nocache: false
+});
+config.files.push({
+   pattern: __dirname + "/src/commonTest/resources/**",
+   watched: false,
+   included: false,
+   served: true,
+   nocache: false
+});
+config.set({
+    "proxies": {
+       "/": __dirname + "/"
+    },
+    "urlRoot": "/__karma__/"
+});
