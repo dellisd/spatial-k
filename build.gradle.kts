f0e6b3cac881a4154ab00bc3a7f17b7e0637118a
Derek Ellis <derekellis170@gmail.com>
2024-05-07 18:38:16 -0400

    Remove detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,7 @@
-import io.gitlab.arturbosch.detekt.Detekt
-
 plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.publish) apply false
-    alias(libs.plugins.detekt)
     alias(libs.plugins.dokka)
-}
-
-detekt {
-    buildUponDefaultConfig = true
-
-    source = files(
-        project(":geojson").projectDir.resolve("src"),
-        project(":turf").projectDir.resolve("src")
-    )
-}
-
-
-tasks.withType<Detekt> {
-    buildUponDefaultConfig = true
-    jvmTarget = "1.8"
-    reports {
-        html.required.set(true)
-    }
 }
 
 tasks.dokkaHtmlMultiModule.configure {
